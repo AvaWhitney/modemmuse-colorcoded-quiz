33a5b2c702fb4939c51e7ef41fade5008473bbd3
Ava Whitney <avawhitney.aw@gmail.com>
2025-04-13 02:46:46 -0700

    added hamburger menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import Quiz from "./components/Quiz.tsx";
+import HamburgerMenu from "./components/HamburgerMenu.tsx";
 
 function App() {
   return (
-    <main className="min-h-screen bg-pink-50 md:bg-cover bg-[25%_95%] text-gray-900" style={{ backgroundImage: "url('/color_coded_bg.png')" }}>
-      <h1 className="max-w-xl mx-auto p-5 md:pt-10 text-center text-2xl sm:text-3xl md:text-4xl font-bold md:font-semibold">Are You Mentally Stable or Just Good at Color-Coding Your Calendar?</h1>
+   
+  
+      <main className="min-h-screen bg-pink-50 md:bg-cover bg-[25%_95%] text-gray-900" style={{ backgroundImage: "url('/color_coded_bg.png')" }}>        
+        <header>
+        <HamburgerMenu />
+      </header>
+      <h1 className="max-w-xl mx-auto p-5 md:pt-4 text-center text-2xl sm:text-3xl md:text-4xl font-bold md:font-semibold">Are You Mentally Stable or Just Good at Color-Coding Your Calendar?</h1>
       
       <Quiz />
     </main>
+   
+    
   );
 }
 
